075df48551772ebb609b5a3affbab291f173392d
Stefano Alberto Russo <stefano.russo@gmail.com>
2017-07-05 16:06:20 +0200

    First working version

---
+++ services/postgres/prestartup_postgres.sh
@@ -0,0 +1,83 @@
+#!/bin/bash
+
+echo 'Executing Postgres Prestartup'
+
+#------------------------------
+# Postgres-specific (data dir)
+#------------------------------
+
+if [ ! -d "/data/postgres/9.3" ]; then
+    echo "Data dir does not exist"
+    # Setup /data/postgres
+    mkdir -p /data/postgres
+    chown postgres:postgres  /data/postgres
+    chmod 700 /data/postgres
+
+    # Setup /data/postgres/9.3
+    mkdir -p /data/postgres/9.3
+    chown postgres:postgres /data/postgres/9.3
+    chmod 700 /data/postgres/9.3
+
+    # Copy files
+    mv /var/lib/postgresql/9.3/main /data/postgres/9.3/
+
+    # Create link
+    ln -s /data/postgres/9.3/main /var/lib/postgresql/9.3/main
+
+    # Move conf
+    mv /etc/postgresql/9.3/main/pg_hba.conf /data/postgres
+    ln -s /data/postgres/pg_hba.conf /etc/postgresql/9.3/main/pg_hba.conf
+    chown postgres:postgres /data/postgres/pg_hba.conf
+
+    # Move conf
+    mv /etc/postgresql/9.3/main/postgresql.conf /data/postgres
+    ln -s /data/postgres/postgresql.conf /etc/postgresql/9.3/main/postgresql.conf
+    chown postgres:postgres /data/postgres/postgresql.conf
+
+else
+    echo "Data dir does exist"
+    mv /var/lib/postgresql/9.3/main /var/lib/postgresql/9.3/main_or
+    ln -s /data/postgres/9.3/main /var/lib/postgresql/9.3/main
+
+    mv /etc/postgresql/9.3/main/pg_hba.conf /etc/postgresql/9.3/main/pg_hba.conf_or
+    ln -s /data/postgres/pg_hba.conf /etc/postgresql/9.3/main/pg_hba.conf
+    chown postgres:postgres /data/postgres/pg_hba.conf
+
+    mv /etc/postgresql/9.3/main/postgresql.conf /etc/postgresql/9.3/main/postgresql.conf_or
+    ln -s /data/postgres/postgresql.conf /etc/postgresql/9.3/main/postgresql.conf
+    chown postgres:postgres /data/postgres/postgresql.conf
+
+fi
+
+
+if [ ! -f /data/postgres/configured_flag ]; then
+    echo 'Configuring as /data/postgres/configured_flag is not found...'   
+    echo "Running postgres server in standalone mode for configuring users..."
+    # Run Postgres
+    /etc/supervisor/conf.d/run_postgres.sh &
+
+    # Save PID
+    PID=$!
+
+    echo "PID=$PID"
+
+    # Wait for postgres to become ready
+    # Should be improved
+    sleep 5
+    874r73
+    echo 'Creating eDjango user/db...'
+    su postgres -c "psql -f /create_eDjango_user.sql"
+
+    # Set configured flag
+    touch /data/postgres/configured_flag
+
+    echo "Stopping Postgres"
+
+    # Stop Postgres
+    kill $PID
+
+    echo "Ok, configured"
+else
+    echo ' Not configuring as /data/postgres/configured_flag is found.'
+fi
+
---
+++ services/postgres/run_postgres.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+# This script is run by Supervisor to start PostgreSQL 9.3 in foreground mode
+# https://github.com/Karumi/docker-sentry/blob/master/conf/run_postgres.sh
+
+if [ -d /var/run/postgresql ]; then
+    chmod 2775 /var/run/postgresql
+else
+    install -d -m 2775 -o postgres -g postgres /var/run/postgresql
+fi
+
+exec su postgres -c "/usr/lib/postgresql/9.3/bin/postgres -D /var/lib/postgresql/9.3/main -c config_file=/etc/postgresql/9.3/main/postgresql.conf"
