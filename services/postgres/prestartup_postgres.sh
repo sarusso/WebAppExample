05fc5ce456b52d8840eb8386f8c283cb0fd767c1
Stefano Alberto Russo <stefano.russo@gmail.com>
2018-07-23 20:59:38 +0200

    Fixed ownership bug in Postgres.

--- services/postgres/prestartup_postgres.sh
+++ services/postgres/prestartup_postgres.sh
@@ -11,12 +11,12 @@ if [ ! -d "/data/postgres/9.3" ]; then
     echo "Data dir does not exist"
     # Setup /data/postgres
     mkdir -p /data/postgres
-    chown reyns:reyns  /data/postgres
+    chown postgres:postgres  /data/postgres
     chmod 700 /data/postgres
 
     # Setup /data/postgres/9.3
     mkdir -p /data/postgres/9.3
-    chown reyns:reyns /data/postgres/9.3
+    chown postgres:postgres /data/postgres/9.3
     chmod 700 /data/postgres/9.3
 
     # Copy files
@@ -28,12 +28,12 @@ if [ ! -d "/data/postgres/9.3" ]; then
     # Move conf
     mv /etc/postgresql/9.3/main/pg_hba.conf /data/postgres
     ln -s /data/postgres/pg_hba.conf /etc/postgresql/9.3/main/pg_hba.conf
-    chown reyns:reyns /data/postgres/pg_hba.conf
+    chown postgres:postgres /data/postgres/pg_hba.conf
 
     # Move conf
     mv /etc/postgresql/9.3/main/postgresql.conf /data/postgres
     ln -s /data/postgres/postgresql.conf /etc/postgresql/9.3/main/postgresql.conf
-    chown reyns:reyns /data/postgres/postgresql.conf
+    chown postgres:postgres /data/postgres/postgresql.conf
 
 else
     echo "Data dir does exist"
@@ -42,19 +42,13 @@ else
 
     mv /etc/postgresql/9.3/main/pg_hba.conf /etc/postgresql/9.3/main/pg_hba.conf_or
     ln -s /data/postgres/pg_hba.conf /etc/postgresql/9.3/main/pg_hba.conf
-    chown reyns:reyns /data/postgres/pg_hba.conf
+    chown postgres:postgres /data/postgres/pg_hba.conf
 
     mv /etc/postgresql/9.3/main/postgresql.conf /etc/postgresql/9.3/main/postgresql.conf_or
     ln -s /data/postgres/postgresql.conf /etc/postgresql/9.3/main/postgresql.conf
-    chown reyns:reyns /data/postgres/postgresql.conf
+    chown postgres:postgres /data/postgres/postgresql.conf
 
 fi
-
-
-# Fix ownerships if needed
-chown -R reyns:reyns /data/postgres/9.3/main
-chown -R reyns:reyns /var/run/postgresql
-chown -R reyns:reyns /data/postgres/
 
 
 # Configure Postgres (create user etc.)
--- services/postgres/run_postgres.sh
+++ services/postgres/run_postgres.sh
@@ -3,28 +3,10 @@
 # This script is run by Supervisor to start PostgreSQL 9.3 in foreground mode
 # https://github.com/Karumi/docker-sentry/blob/master/conf/run_postgres.sh
 
-# Check if Firewall is set up. If not, keep waiting.
-#while true
-#do
-#    echo "Checking Firewall setup..."    
-#    if [  -f /run/fw_ready ]; then
-#        echo "Ok, Firewall is set up."
-#        break
-#    fi
-#    sleep 5
-#done
-
-if [[ "x$DISABLE_FIREWALL" == "xTrue" ]] ; then
-    echo "Not enabling firewall"
-else
-    # Setup firewall (we can do it here as we are root)
-    sudo /setup_firewall.sh
-fi
-
 if [ -d /var/run/postgresql ]; then
     chmod 2775 /var/run/postgresql
 else
     install -d -m 2775 -o postgres -g postgres /var/run/postgresql
 fi
 
-exec su reyns -c "/usr/lib/postgresql/9.3/bin/postgres -D /var/lib/postgresql/9.3/main -c config_file=/etc/postgresql/9.3/main/postgresql.conf"
+exec su postgres -c "/usr/lib/postgresql/9.3/bin/postgres -D /var/lib/postgresql/9.3/main -c config_file=/etc/postgresql/9.3/main/postgresql.conf"
