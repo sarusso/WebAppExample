d9b24e00262e94fc701d976096998697402fa1f4
Stefano Alberto Russo <stefano.russo@gmail.com>
2018-07-26 12:32:48 +0200

    Updated to Postgres 10.

--- services/postgres/prestartup_postgres.sh
+++ services/postgres/prestartup_postgres.sh
@@ -7,58 +7,58 @@ echo 'Executing Postgres Prestartup'
 # Postgres-specific (data dir)
 #------------------------------
 
-if [ ! -d "/data/postgres/9.3" ]; then
+if [ ! -d "/data/postgres/10" ]; then
     echo "Data dir does not exist"
     # Setup /data/postgres
     mkdir -p /data/postgres
     chown postgres:postgres  /data/postgres
     chmod 700 /data/postgres
 
-    # Setup /data/postgres/9.3
-    mkdir -p /data/postgres/9.3
-    chown postgres:postgres /data/postgres/9.3
-    chmod 700 /data/postgres/9.3
+    # Setup /data/postgres/10
+    mkdir -p /data/postgres/10
+    chown postgres:postgres /data/postgres/10
+    chmod 700 /data/postgres/10
 
     # Copy files
-    mv /var/lib/postgresql/9.3/main /data/postgres/9.3/
+    mv /var/lib/postgresql/10/main /data/postgres/10/
 
     # Create link
-    ln -s /data/postgres/9.3/main /var/lib/postgresql/9.3/main
+    ln -s /data/postgres/10/main /var/lib/postgresql/10/main
 
     # Move conf
-    mv /etc/postgresql/9.3/main/pg_hba.conf /data/postgres
-    ln -s /data/postgres/pg_hba.conf /etc/postgresql/9.3/main/pg_hba.conf
+    mv /etc/postgresql/10/main/pg_hba.conf /data/postgres
+    ln -s /data/postgres/pg_hba.conf /etc/postgresql/10/main/pg_hba.conf
     chown postgres:postgres /data/postgres/pg_hba.conf
 
     # Move conf
-    mv /etc/postgresql/9.3/main/postgresql.conf /data/postgres
-    ln -s /data/postgres/postgresql.conf /etc/postgresql/9.3/main/postgresql.conf
+    mv /etc/postgresql/10/main/postgresql.conf /data/postgres
+    ln -s /data/postgres/postgresql.conf /etc/postgresql/10/main/postgresql.conf
     chown postgres:postgres /data/postgres/postgresql.conf
 
 else
     echo "Data dir does exist"
-    mv /var/lib/postgresql/9.3/main /var/lib/postgresql/9.3/main_or
-    ln -s /data/postgres/9.3/main /var/lib/postgresql/9.3/main
+    mv /var/lib/postgresql/10/main /var/lib/postgresql/10/main_or
+    ln -s /data/postgres/10/main /var/lib/postgresql/10/main
 
-    mv /etc/postgresql/9.3/main/pg_hba.conf /etc/postgresql/9.3/main/pg_hba.conf_or
-    ln -s /data/postgres/pg_hba.conf /etc/postgresql/9.3/main/pg_hba.conf
+    mv /etc/postgresql/10/main/pg_hba.conf /etc/postgresql/10/main/pg_hba.conf_or
+    ln -s /data/postgres/pg_hba.conf /etc/postgresql/10/main/pg_hba.conf
     chown postgres:postgres /data/postgres/pg_hba.conf
 
-    mv /etc/postgresql/9.3/main/postgresql.conf /etc/postgresql/9.3/main/postgresql.conf_or
-    ln -s /data/postgres/postgresql.conf /etc/postgresql/9.3/main/postgresql.conf
+    mv /etc/postgresql/10/main/postgresql.conf /etc/postgresql/10/main/postgresql.conf_or
+    ln -s /data/postgres/postgresql.conf /etc/postgresql/10/main/postgresql.conf
     chown postgres:postgres /data/postgres/postgresql.conf
 
 fi
 
 # Check correct permissions. Incorrect permissions might occur when changing base images,
 # as the user "postgres" might get mapped to a differend uid / guid.
-PERMISSIONS=$(ls -alh /data/postgres/9.3 | grep main | awk '{print $3 ":" $4}')
+PERMISSIONS=$(ls -alh /data/postgres/10 | grep main | awk '{print $3 ":" $4}')
 if [[ "x$PERMISSIONS" == "xpostgres:postgres" ]] ; then
     # Everything ok
     :
 else
     # Fix permissions
-    chown -R postgres:postgres /data/postgres/9.3 
+    chown -R postgres:postgres /data/postgres/10 
     chown -R postgres:postgres /data/postgres/pg_hba.conf  
     chown -R postgres:postgres /data/postgres/postgresql.conf
     chown -R postgres:postgres /var/run/postgresql
--- services/postgres/run_postgres.sh
+++ services/postgres/run_postgres.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# This script is run by Supervisor to start PostgreSQL 9.3 in foreground mode
+# This script is run by Supervisor to start PostgreSQL 10 in foreground mode
 # https://github.com/Karumi/docker-sentry/blob/master/conf/run_postgres.sh
 
 if [ -d /var/run/postgresql ]; then
@@ -9,4 +9,4 @@ else
     install -d -m 2775 -o postgres -g postgres /var/run/postgresql
 fi
 
-exec su postgres -c "/usr/lib/postgresql/9.3/bin/postgres -D /var/lib/postgresql/9.3/main -c config_file=/etc/postgresql/9.3/main/postgresql.conf"
+exec su postgres -c "/usr/lib/postgresql/10/bin/postgres -D /var/lib/postgresql/10/main -c config_file=/etc/postgresql/10/main/postgresql.conf"
