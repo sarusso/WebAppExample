d45133aba3272c4cf286e768e2df07704d38488a
Stefano Alberto Russo <stefano.russo@gmail.com>
2018-11-20 13:20:31 +0100

    Improved logging, updated eDjango and added support for custom security key, bugfixes.

--- services/web/run_web.sh
+++ services/web/run_web.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 
-# Do not use a "set -e" here or it will fail on some specific wrong lines in loading the env.
+DATE=$(date)
+
+echo ""
+echo "===================================================="
+echo "  Starting WebApp @ $DATE"
+echo "===================================================="
+echo ""
+
+echo "1) Loading/sourcing env and settings"
 
 # Load Reyns env
 source /env.sh
@@ -12,8 +20,8 @@ cd /opt/web
 source db_conf.sh
 
 # Set log levels
-export EDJANGO_LOG_LEVEL=DEBUG
-export DJANGO_LOG_LEVEL=ERROR
+export EDJANGO_LOG_LEVEL=INFO
+export DJANGO_LOG_LEVEL=INFO
 
 # eDjango Project conf
 export EDJANGO_EMAIL_SERVICE="Sendgrid"
@@ -22,16 +30,55 @@ export EDJANGO_EMAIL_APIKEY=""
 export EDJANGO_PROJECT_NAME="WebApp"
 export EDJANGO_PUBLIC_HTTP_HOST="https://localhost"
 
+export DJANGO_SECRET_KEY='#k%899hw@w%1((_&=640-4w#p)fret$m4%#(9x^+it5(h1b6zy'
+
 # To Python3
 export EDJANGO_PYTHON=python3
 
 # Set Python unbuffered or some error messages might not appear in logs
 export PYTHONUNBUFFERED=1
 
-# Init
-cd /opt/web/eDjango/ && fab makemigrations &>> #/var/log/web/makemigrations.log
-cd /opt/web/eDjango/ && fab install:noinput=True #&>> /var/log/web/install.log
-cd /opt/web/eDjango/ && fab populate #&>> /var/log/web/populate.log
+# Stay quiet
+export PYTHONWARNINGS=ignore
 
-# Run the development server
-exec fab runserver
+# Check if there is something to migrate or populate
+echo ""
+echo "2) Making migrations..."
+cd /opt/web/eDjango/ && fab makemigrations:noinput=True
+EXIT_CODE=$?
+echo "Exit code: $EXIT_CODE"
+if [[ "x$EXIT_CODE" != "x0" ]] ; then
+    echo "This exit code is an error, sleeping 5s and exiting." 
+    sleep 5
+    exit $?
+fi
+echo ""
+
+echo "3) Migrating..."
+cd /opt/web/eDjango/ && fab migrate:noinput=True
+EXIT_CODE=$?
+echo "Exit code: $EXIT_CODE"
+if [[ "x$EXIT_CODE" != "x0" ]] ; then
+    echo "This exit code is an error, sleeping 5s and exiting." 
+    sleep 5
+    exit $?
+fi
+echo ""
+
+echo "4) Populating..."
+cd /opt/web/eDjango/ && fab populate
+EXIT_CODE=$?
+echo "Exit code: $EXIT_CODE"
+if [[ "x$EXIT_CODE" != "x0" ]] ; then
+    echo "This exit code is an error, sleeping 5s and exiting." 
+    sleep 5
+    exit $?
+fi
+echo ""
+
+# Run the (development) server
+echo "5) Now starting the server and logging in /var/log/web/server.log."
+exec fab runserver 2>> /var/log/web/server.log
+
+
+
--- view_web_server_log.sh
+++ view_web_server_log.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
 tail -f -n 1000 data_webappexample/web-published/log/web/server.log
+
